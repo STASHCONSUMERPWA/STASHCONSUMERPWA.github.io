092816b86059cfad6c70d94c7748bf06ae5bf7d2
CONSUMER PWA STASH Dev <consumerpwa.stashdev@stash.asia>
2019-02-18 22:01:37 +0800

    Update 20180218_8

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,14 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+
+  workbox.precaching.precacheAndRoute([]);
+
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
 workbox.setConfig({
   debug: false,
 });
